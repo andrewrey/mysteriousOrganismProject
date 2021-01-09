f4c94ad4e2f56deabb087538aea23e19c44cc1e7
Andrew Reynolds <andrew_reynolds@mac.com>
2021-01-08 17:59:24 -0800

    added compare functionality to print statement of percentage of similarity

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const returnRandBase = () => {
   
 // Factory function 
 
-const pAequorFactory = (num, dnaArray) => {
+const pAequorFactory = (num, dnaArray=mockUpStrand()) => {
     return {
         num,
         dnaArray,
@@ -30,13 +30,22 @@ const pAequorFactory = (num, dnaArray) => {
             this.dnaArray.splice(indexToMutate,1,characterReplacement);
             return this.dnaArray;
         },
-        
+        compare(pObject){
+            let counter = 0;
+            this.dnaArray.forEach((item, index) => {
+                this.dnaArray[index] === pObject.dnaArray[index]? counter++: null;
+            })
+            console.log(counter);
+            let percentageSame = counter / this.dnaArray.length;
+            return `Specimen #${this.num} and specimen #${pObject.num} have ${Math.round(percentageSame * 100)}% DNA in common.`; 
+        }, 
     };
 };
   
   
-  let first = pAequorFactory(33, mockUpStrand());
+ let first = pAequorFactory(1);
+ let second = pAequorFactory(2);
 
-  console.log(first);
-  first.mutate();
-  console.log(first.dnaArray);
+ console.log(first);
+ console.log(second);
+ console.log(first.compare(second));
